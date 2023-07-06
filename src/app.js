199bcb05b681b48ec8309aa933a2addc6b98cf7a
user <frontendreactjs@gmail.com>
2023-07-06 13:46:14 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+VANTA.BIRDS({
+    el: "#animated-bg",
+    scaleMobile: 1.00,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x4e7cb9,
+    color1: 0x7e97ed,
+    color2: 0x144557
+});
+
+VANTA.GLOBE({
+    el: "#animated-globe",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xffffff,
+    backgroundColor: 0xafc3ff
+  })
