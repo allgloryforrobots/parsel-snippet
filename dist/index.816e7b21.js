1435a49ed79eb26f47282ccbcf58950bee429f0e
FrontendReactJS <allgloryforrobots@gmail.com>
2023-08-31 12:37:07 +0300

    'f'

--- dist/index.816e7b21.js
+++ dist/index.816e7b21.js
@@ -1,5 +1,5 @@
-VANTA.BIRDS({
-    el: "#animated-birds",
+let anim = VANTA.WAVES({
+    el: "#animated-wave",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
@@ -7,31 +7,14 @@ VANTA.BIRDS({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    backgroundColor: 0x4e7cb9,
-    color1: 0x7e97ed,
-    color2: 0x144557
+    color: 0x2a4089
 });
-VANTA.GLOBE({
-    el: "#animated-globe",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xffffff,
-    backgroundColor: 0xafc3ff
-});
-const block_1 = $(".block-1");
-const block_2 = $(".block-2");
-onmousemove = (e)=>{
-    const x = innerWidth / 2 - e.x;
-    const y = innerHeight / 2 - e.y;
-    block_1.css("transform", `translate(${x / 50}px, 0)`);
-    block_2.css("transform", `translate(${-x / 40}px, 0)`);
-//   block_1.style.transform = `translate(${x / 50}px, 0)`
-//   block_2.style.transform = `translate(${-x / 40}px, 0)`
-};
+// в vanta.waves.mod.min.js анимация остановлена на 1м кадре
+// const canvas = anim.renderer.domElement;
+const canvas = document.querySelector(".vanta-canvas");
+setTimeout(()=>{
+    const fullQuality = canvas.toDataURL();
+    console.log("fullQuality2", fullQuality);
+}, 1000);
 
 //# sourceMappingURL=index.816e7b21.js.map
--- src/app.js
+++ src/app.js
@@ -1,42 +1,23 @@
 
-VANTA.BIRDS({
-    el: "#animated-birds",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundColor: 0x4e7cb9,
-    color1: 0x7e97ed,
-    color2: 0x144557
+let anim = VANTA.WAVES({
+  el: "#animated-wave",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x2a4089
 });
 
-VANTA.GLOBE({
-    el: "#animated-globe",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xffffff,
-    backgroundColor: 0xafc3ff
-})
+// в vanta.waves.mod.min.js анимация остановлена на 1м кадре
 
 
-const block_1 = $('.block-1')
-const block_2 = $('.block-2')
+// const canvas = anim.renderer.domElement;
+const canvas = document.querySelector(".vanta-canvas");
 
-onmousemove = (e) => {
-  const x = innerWidth / 2 - e.x
-  const y = innerHeight / 2 - e.y
-  block_1.css("transform", `translate(${x / 50}px, 0)`)
-  block_2.css("transform", `translate(${-x / 40}px, 0)`)
-
-
-//   block_1.style.transform = `translate(${x / 50}px, 0)`
-//   block_2.style.transform = `translate(${-x / 40}px, 0)`
-}
+setTimeout(() => {
+  const fullQuality = canvas.toDataURL();
+  console.log("fullQuality2", fullQuality)
+}, 1000);
